7737ef47e087ba5ee533009707d4e430e8cc5074
Шабанов Касум <shabanov.kasum@list.ru>
2021-10-03 19:43:48 +0300

    first commit mole-game

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -5,25 +5,25 @@ holesArray = [];
 
 (function addHolles(holesArray) {
   const holes = document.getElementsByClassName('hole-name');
-  for(let i = 1; i <= holes[0].childElementCount; ++){
+  for (let i = 1; i <= holes[0].childElementCount; ++) {
     const hole = document.getElementById(getHole(i));
     holesArray.push(hole.addEventListener('click', () => {
       hole.className.incudes('hole_has_mole') ?
-          dead.textContent++;
-          lost.textContent++;
+        dead.textContent++;
+      lost.textContent++;
     }))
   }
 });
 
-function isWinner(){
+function isWinner() {
   const winner = 10;
   const loser = 5;
 
-  if(Number(dead.textContent) == winner){
+  if (Number(dead.textContent) == winner) {
     alert('Победа!');
     dead.textContent = 0;
     lost.textContent = 0;
-  } else(Number(lost.textContent) == loser){
+  } else (Number(lost.textContent) == loser){
     alert('Вы проиграли!');
     dead.textContent = 0;
     lost.textContent = 0;
@@ -31,9 +31,9 @@ function isWinner(){
 };
 
 let id = setInterval(() => {
-  isWinner(dead,lost)
+  isWinner(dead, lost)
 }, 100);
 
-function getHole(index){
-  return`hole${index}`
+function getHole(index) {
+  return `hole${index}`
 };
