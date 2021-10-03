a83447b17038b7f69d42f0edf5bec644611ab0a6
Шабанов Касум <shabanov.kasum@list.ru>
2021-10-03 09:11:21 +0300

    first commit popups

---
+++ js-features/mole-game/task.js
@@ -0,0 +1,39 @@
+const dead = document.getElementById('dead');
+const lost = document.getElementById('lost');
+
+holesArray = [];
+
+(function addHolles(holesArray) {
+  const holes = document.getElementsByClassName('hole-name');
+  for(let i = 1; i <= holes[0].childElementCount; ++){
+    const hole = document.getElementById(getHole(i));
+    holesArray.push(hole.addEventListener('click', () => {
+      hole.className.incudes('hole_has_mole') ?
+          dead.textContent++;
+          lost.textContent++;
+    }))
+  }
+});
+
+function isWinner(){
+  const winner = 10;
+  const loser = 5;
+
+  if(Number(dead.textContent) == winner){
+    alert('Победа!');
+    dead.textContent = 0;
+    lost.textContent = 0;
+  } else(Number(lost.textContent) == loser){
+    alert('Вы проиграли!');
+    dead.textContent = 0;
+    lost.textContent = 0;
+  };
+};
+
+let id = setInterval(() => {
+  isWinner(dead,lost)
+}, 100);
+
+function getHole(index){
+  return`hole${index}`
+};
